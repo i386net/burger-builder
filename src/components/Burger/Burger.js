40b1958d3da0220d64907205d2f9b7dda48a591d
DK <i386.net@gmail.com>
2020-11-29 17:21:56 +0300

    update Burger: • flat ingredients array • add render logic if ingredients array is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,18 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = ({ ingredients }) => {
   // получаем массив из переданных ингредиентов
-  const transformedIngredients = Object.keys(ingredients)
+  let transformedIngredients = Object.keys(ingredients)
     .map( igKey => {
       return [...Array(ingredients[igKey])].map((_, i) => {
        return  <BurgerIngredient key={igKey + i} type={igKey}/>;
       });
-    });
-
+    })
+    .flat(); // делаем массив плоским!
+    // .reduce((arr, el) => arr.concat(el), []);
+  console.log(transformedIngredients)
+  if(transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type={'bread-top'} />
