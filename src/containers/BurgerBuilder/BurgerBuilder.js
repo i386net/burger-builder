8f3f02c07142f798f097b14455ef7c059abb8bd0
DK <i386.net@gmail.com>
2020-11-29 17:22:39 +0300

    update BurgerBuilder: • add state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
 
 export default class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      meat: 0,
+      cheese: 0,
+    }
+  }
+
+  // ingredientsHandler = (ingredients) => {
+  //   if(Object.values(ingredients).every(ingredient => ingredient === 0)) {
+  //     return <p>Add some ingredients!</p>
+  //   }
+  //   return <Burger ingredients={ingredients}/>;
+  // }
 
   render() {
+    const { ingredients } = this.state;
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger ingredients={ingredients}/>
+        {/*{this.ingredientsHandler(ingredients)}*/}
         <div>Build controls</div>
       </Aux>
     )
