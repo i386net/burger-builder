4446cbfb231c6511ca4e0464cf4df31599a6bb9f
DK <i386.net@gmail.com>
2020-12-06 09:02:06 +0300

    BurgerBuilder add: INGREDIENT_PRICES const add: totalPrice state add: add addIngredientHandler update: BuildControls add ingredientAdded props

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,13 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  bacon: 0.7,
+  meat: 1.5,
+  cheese: 0.5,
+};
+
 export default class BurgerBuilder extends Component {
   state = {
     ingredients: {
@@ -10,22 +17,34 @@ export default class BurgerBuilder extends Component {
       bacon: 0,
       meat: 0,
       cheese: 0,
-    }
+    },
+    totalPrice: 4,
   }
 
-  // ingredientsHandler = (ingredients) => {
-  //   if(Object.values(ingredients).every(ingredient => ingredient === 0)) {
-  //     return <p>Add some ingredients!</p>
-  //   }
-  //   return <Burger ingredients={ingredients}/>;
-  // }
+  addIngredientHandler = (type) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {...this.state.ingredients};
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: newPrice,
+    })
+  }
+
+  removeIngredientHandler = (type) => {
+
+  }
 
   render() {
     const { ingredients } = this.state;
     return (
       <Aux>
         <Burger ingredients={ingredients}/>
-        <BuildControls />
+        <BuildControls ingredientAdded = {this.addIngredientHandler}/>
       </Aux>
     )
   }
