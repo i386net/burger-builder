8ec526262e5cd47643d815adfcb98ebabac36294
DK <i386.net@gmail.com>
2020-12-06 09:56:42 +0300

    BuildControl
    update: add `disable` prop

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,8 +5,15 @@ const BuildControl = (props) => {
   return (
     <div className={styles.BuildControl}>
       <div className={styles.Label}>{props.label}</div>
-      <button className={styles.Less}>➖</button>
-      <button className={styles.More} onClick={props.added}>➕</button>
+      <button
+        className={styles.Less}
+        onClick={props.removed}
+        disabled={props.disabled}
+      >➖</button>
+      <button
+        className={styles.More}
+        onClick={props.added}
+      >➕</button>
     </div>
   )
 }
