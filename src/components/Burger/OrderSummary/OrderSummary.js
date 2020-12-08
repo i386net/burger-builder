cdd73cf39420960dd03bfd9d0b82066430779068
DK <i386.net@gmail.com>
2020-12-08 08:56:23 +0300

    OrderSummary.js
    add: Cancel and Continue buttons

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
 import INGREDIENT_PRICES from '../../../constants/ingridient-prices';
+import Button from '../../UI/Button/Button';
+
 
 const OrderSummary = (props) => {
   const ingredientsSummary = Object.entries(props.ingredients).map(([ingredient, value]) => {
@@ -17,6 +19,8 @@ const OrderSummary = (props) => {
       </ul>
       <p>{props.price.toFixed(2)}</p>
       <p>Continue to checkout?</p>
+      <Button buttonType={'Danger'} clicked={props.purchaseCancel}>Cancel</Button>
+      <Button buttonType={'Success'} clicked={props.purchaseContinue}>Continue</Button>
     </Aux>
   )
 }
