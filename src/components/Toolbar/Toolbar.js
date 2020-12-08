cfb0a77ddee73c736ee003eca1cf401ee280954f
DK <i386.net@gmail.com>
2020-12-08 20:28:24 +0300

    import Logo

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Toolbar.module.css';
+import Logo from '../Logo/Logo';
 
 const Toolbar = () => {
   return (
     <header className={styles.Toolbar}>
+      <Logo />
       <div>MENU</div>
-      <div>LOGO</div>
       <nav className={styles.Toolbar__nav}>...</nav>
 
     </header>
