9e6c0e275c2c16af2580a41aea35a5abb1eadfad
DK <i386.net@gmail.com>
2020-12-14 20:55:33 +0300

    Layout
    update: convert into a class component
    add: state
    add: state handler

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.css';
 import Toolbar from '../Toolbar/Toolbar';
+import Sidebar from '../Sidebar/Sidebar';
 
-const layout = (props) => {
-  return (
-    <Aux>
-      {/*<div>Toolbar, SideDrawer, Backdrop</div>*/}
-      <Toolbar />
-      <main className={styles.Content}>
-        {props.children}
-      </main>
-    </Aux>
-  )
+export default class Layout extends Component {
+  state = {
+    showSideBar: true,
+  }
+  sideBarClosedHandler = () => {
+    this.setState({
+      showSideBar: false,
+    })
+  }
+
+
+  render() {
+    return (
+      <Aux>
+        <Sidebar
+          closed={this.sideBarClosedHandler}
+          open={this.state.showSideBar} />
+        <Toolbar />
+        <main className={styles.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
 }
-
-export default layout;
