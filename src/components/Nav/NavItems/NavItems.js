ad65bd5a88d96a2ebbaed85407feae0287d5d7ec
DK <i386.net@gmail.com>
2020-12-14 20:52:37 +0300

    NavItems
    add props to handle nav items in sidebar

--- src/components/Nav/NavItems/NavItems.js
+++ src/components/Nav/NavItems/NavItems.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './NavItems.module.css'
 import NavItem from './NavItem/NavItem';
 
-const NavItems = () => {
+const NavItems = ({ column }) => {
   return (
-    <ul className={styles.NavItems}>
+    <ul className={column? [styles.NavItems, styles.NavItems_column].join(' '):styles.NavItems}>
       <NavItem link='#' active>Burger Builder</NavItem>
       <NavItem link='#'>Checkout</NavItem>
     </ul>
